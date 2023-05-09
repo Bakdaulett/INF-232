f7e7125858c59a2b908e0aed4f59c069d15d2b6b
yerassb <erasberdimurat2004@gmail.com>
2023-05-09 14:40:16 +0600

    Created Models

--- Quiz/quizapp/admin.py
+++ Quiz/quizapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Creator)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Variant)
--- Quiz/quizapp/models.py
+++ Quiz/quizapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=255)
+
+class Creator(models.Model):
+    creator_id = models.IntegerField(primary_key=True)
+    email = models.EmailField()
+    username = models.CharField(max_length=255)
+
+class Quiz(models.Model):
+    quiz_id = models.IntegerField(primary_key=True)
+    topic = models.CharField(max_length=255)
+    creator_id = models.ForeignKey(Creator, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    qa_id = models.IntegerField(primary_key=True)
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+class Variant(models.Model):
+    v_id = models.IntegerField(primary_key=True)
+    variant = models.CharField(max_length=255)
+    qa_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+
+
